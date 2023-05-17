703055b4f19eccff73c8ee893e01e96ef7f1646b
ouafaema <marhrouf.ouafae@gmail.com>
2023-05-17 11:34:10 +0200

    bonus added

---
+++ src/components/PokemonList/PokemonCard.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const PokemonCard = ({ url }) => {
+    const [pokemon, setPokemon] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(url);
+            setPokemon(result.data);
+        };
+        fetchData();
+    }, [url]);
+
+    if (!pokemon) return null;
+
+    return (
+        <div>
+            <h3>{pokemon.name}</h3>
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        </div>
+    );
+}
+
+export default PokemonCard;
